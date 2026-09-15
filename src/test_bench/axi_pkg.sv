66ee7bb448dd2f959936f0e758d1c65fb939c4c4
savikshaja <savikshaja@gmail.com>
2026-09-15 11:43:33 +0530

    initial

---
+++ src/test_bench/axi_pkg.sv
@@ -0,0 +1,20 @@
+// Package file
+package axi_pkg;
+
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+  `include "defines.svh"
+  `include "axi_seq_item.sv"
+  `include "axi_sequencer.sv"
+  `include "axi_sequences.sv"
+  `include "axi_driver.sv"
+  `include "axi_ip_monitor.sv"
+  `include "axi_op_monitor.sv"
+  `include "axi_subscriber.sv"
+  `include "axi_scoreboard.sv"
+  `include "axi_ip_agent.sv"
+  `include "axi_op_agent.sv"
+  `include "axi_env.sv"
+  `include "axi_test.sv"
+
+endpackage
