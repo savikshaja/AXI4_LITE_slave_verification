a620f892806d9b779590cd235b87b7c4f537bbe0
savikshaja <savikshaja@gmail.com>
2026-09-15 11:54:15 +0530

    initial

---
+++ src/test_bench/defines.sv
@@ -0,0 +1,7 @@
+`ifndef DEFINES_SV
+`define DEFINES_SV
+
+`define DW 32          // Data width  (matches DUT's DATA_WIDTH)
+`define AW 32          // Address width (matches DUT's ADDR_WIDTH)
+`define MEM_DEPTH 16 
+`endif
