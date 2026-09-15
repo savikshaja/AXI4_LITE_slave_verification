be26b72f9d54fb05b2ac473ff4f5c6edd38600c0
savikshaja <savikshaja@gmail.com>
2026-09-15 11:46:25 +0530

    initial

---
+++ src/test_bench/axi_sequencer.sv
@@ -0,0 +1,9 @@
+class axi_sequencer extends uvm_sequencer #(axi_seq_item); 
+
+  `uvm_component_utils(axi_sequencer) 
+
+  function new(string name = "axi_sequencer", uvm_component parent = null); 
+    super.new(name, parent); 
+  endfunction 
+
+endclass
